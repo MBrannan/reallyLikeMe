fe1b995c8c762498e8e54ee7c70a8dbdb4995d85
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-09 17:56:36 -0600

    Add likes get route. Add likes to DOM

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var bios = [{name: "Andrew", bio: "blah", image: "handsomeAndrew.jpeg"},
-            {name: "Michael", bio: "I indulge in the dankest memes", image: "handsomeAndrew.jpeg"},
-            {name: "Jason", bio: "bleg", image: "handsomeAndrew.jpeg"}
+var bios = [{name: "Andrew", bio: "blah", image: "handsomeAndrew.jpeg", likes: 0},
+            {name: "Michael", bio: "I indulge in the dankest memes", image: "handsomeAndrew.jpeg", likes: 0},
+            {name: "Jason", bio: "bleg", image: "handsomeAndrew.jpeg", likes: 0}
 ];
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,6 +14,14 @@ app.set('port', process.env.PORT || 3000);
 
 app.get('/bios', function(req, res){
   res.send(bios);
+});
+
+app.get('/likes', function(req, res) {
+  var likes = {};
+  for (var i = 0; i < bios.length; i++) {
+    likes[bios[i].name] = bios[i].likes;
+  }
+  res.send(likes);
 });
 
 //Static Routes
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,6 +9,14 @@ $(document).ready(function() {
     }
   });
 
+  $.ajax({
+    type: 'GET',
+    url: '/likes',
+    success: function(data) {
+      updateLikes(data);
+    }
+  });
+
 });
 
 function appendToDom(bios) {
@@ -17,7 +25,14 @@ function appendToDom(bios) {
       var $el = $("#bioContainer").children().last();
       $el.append("<h2>" + bios[i].name + "</h2>");
       $el.append("<p>" + bios[i].bio + "</p>");
+      $el.append("<button type='button'>Like " + bios[i].name + "'s profile?</button><span id='" + bios[i].name + "Likes'></span>");
       $el.append("<img src='../assets/images/" + bios[i].image + "' alt='Photo of " + bios[i].name + "'/>");
-      $el.append("<button type='button'>Like " + bios[i].name + "'s profile?</button>");
     }
+}
+
+function updateLikes(likes) {
+  var names = Object.keys(likes);
+  for (var i = 0; i < names.length; i++) {
+    $('#' + names[i] + 'Likes').append(likes[names[i]]);
+  }
 }
