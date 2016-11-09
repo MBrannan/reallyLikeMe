be5bf7fb5873da06b92644f18b1a498607aa9f85
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-09 17:24:20 -0600

    working appends, buttons, bios

--- server/app.js
+++ server/app.js
@@ -3,19 +3,27 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var bios = [{name: "Andrew", bio: "blah", image: "handsomeAndrew.jpeg"},
+            {name: "Michael", bio: "I indulge in the dankest memes", image: "handsomeAndrew.jpeg"},
+            {name: "Jason", bio: "bleg", image: "handsomeAndrew.jpeg"}
+];
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', process.env.PORT || 3000);
 
-//Static Routes
+app.get('/bios', function(req, res){
+  res.send(bios);
+});
 
+//Static Routes
 app.get("/*", function(req, res) {
   var file = req.params[0] || "/views/index.html";
   console.log(req.params);
 
   res.sendFile(path.join(__dirname, "/public/", file))
 });
+
 app.listen(app.get('port'), function() {
   console.log("listening on port " + app.get('port'));
 });
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
   console.log("Tindernauts!");
+
+  $.ajax({
+    type: 'GET',
+    url: '/bios',
+    success: function(data) {
+      appendToDom(data);
+    }
+  });
+
 });
+
+function appendToDom(bios) {
+    for (var i = 0; i < bios.length; i++) {
+      $("#bioContainer").append("<div class='bio'></div>");
+      var $el = $("#bioContainer").children().last();
+      $el.append("<h2>" + bios[i].name + "</h2>");
+      $el.append("<p>" + bios[i].bio + "</p>");
+      $el.append("<img src='../assets/images/" + bios[i].image + "' alt='Photo of " + bios[i].name + "'/>");
+      $el.append("<button type='button'>Like " + bios[i].name + "'s profile?</button>");
+    }
+}
